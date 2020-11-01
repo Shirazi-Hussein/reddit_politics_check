56d52360138bb30a6cca5a22d729ed620629ed60
Hussein Shirazi <41455185+Shirazi-Hussein@users.noreply.github.com>
2020-11-01 12:33:59 -0500

    Add files via upload

--- reddit project/redditdata.py
+++ reddit project/redditdata.py
@@ -7,6 +7,8 @@ Created on Sun Oct 25 19:28:17 2020
 import praw
 import subreddit_list
 from collections import Counter
+from multiprocessing import Process
+
 
 
 class PoliticsStatistics:
@@ -16,7 +18,7 @@ class PoliticsStatistics:
         reddit = praw.Reddit(
             client_id="regAkCxBDMi1fw",
             client_secret="cwEIsCDiQeZAmFskRU9iRLOF51s",
-            user_agent="my user agent"
+            user_agent="windows:politicalbreakdownbot:v1.0.0 (by /u/God_i_hate_today)"
             )
         return reddit
 
@@ -33,22 +35,39 @@ class PoliticsStatistics:
         submission = self.retrieve_usernames()
         all_comments = submission.comments.list()
         usernames = {str(comment.author) for comment in all_comments}
+        usernames = list(usernames)
+        if len(usernames) > 100:
+            usernames = usernames[:100]
         return usernames
     
     
-    def users_top_10(self):
+    def users_top_10(self, username):
         client = self.credentials()
-        usernames = self.store_usernames()
+        global user_data
         user_data = []
-        for username in usernames:
-            user = praw.reddit.Redditor(client, username)
-            history = [post.subreddit.display_name for post in user.new(limit=None)]
-            counts = Counter(history).most_common(10)
-            user_data.append({username:dict(counts)})
+        user = praw.reddit.Redditor(client, username)
+        history = [post.subreddit.display_name for post in user.new(limit=None)]
+        counts = Counter(history).most_common(10)
+        counts = [item[0] for item in counts[2:]]
+        user_data.append({username:counts})
+        
+#test
+if __name__ == "__main__":
+    t = PoliticsStatistics()
+# =============================================================================
+#     for username in t.store_usernames():
+#         p = Process(target=t.users_top_10, args=(username,))
+#         p.start()
+#         p.join()
+#     print("completed")
+# =============================================================================
+    for username in t.store_usernames():
+        t.users_top_10(username)
+        print('done')
+    print('--------------------completed job----------------------------------')
         
 
-t = PoliticsStatistics()
-print(len(t.store_usernames()))
+
             
 
 
